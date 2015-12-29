feb9b6b78670567bae952923b4cf9d9bd4312f20
sohamnavadiya <sohamnavadiya26@gmail.com>
2015-12-29 19:06:55 +0530

    MongoDB Connection Using JAVA

---
+++ mongoDB/src/mongodb/mongoDBConnection.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mongodb;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+import com.mongodb.WriteConcern;
+
+/**
+ *
+ * @author soham
+ */
+public class mongoDBConnection {
+     public static void main(String[] args) {
+    try {
+      MongoClient mongoClient = new MongoClient("localhost", 27017);
+      mongoClient.setWriteConcern(WriteConcern.JOURNAL_SAFE);
+      DB db = mongoClient.getDB("words");
+      DBCollection collection = db.getCollection("word_stats");
+      System.out.println("Number of Documents: " + 
+        new Long(collection.count()).toString());
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
+    
+}
